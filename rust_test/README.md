6bb96377b057d9c68cdccc7a0e3f6064ba6660c5
ltj <1226525682@qq.com>
2022-10-12 18:09:08 +0800

    add loop Option Result test

--- rust_test/README.md
+++ rust_test/README.md
@@ -8,6 +8,15 @@
 - 不使用return关键字返回值的时候不能写分号
 - 数组同python中的list一致不过大小固定 let a = [5; 5]; [T;size]，索引越界时编译就会报错
 - 使用vec!宏来定义矢量，矢量大小可变 let a = vec![5;5]; 索引越界的时候编译不会出错但在运行时候就会报错
+- 使用std::collections::HashMap 中的 HasHMap::new()来定义哈希表
+- 使用insert方法来添加元素, get方法来获取元素,使用map-like操作insert,HashMap.get(KEY)的话，还没有KEY时会返回None, removes方法移除元素
+- 循环使用 loop while for，for可以循环一个范围 for i in 0..5,for i in 0..=5.如果你想要得到数组，可以使用vecy[range]  vector[0..4]，break 来退出且可以显示的返回一个值
+- Option<T>来支持可选的值 Some(T) None
+- match关键字来分别指定Some和None的操作，Some(&值)来特定化情况的输出
+- if let 可以在没有完整模式的时候简化 match，等同于python中if 跟海象表达式一起使用
+- unwrap 默认返回 Some 里的值，如果是 None 将会 panic 并退出程序，expect类似只是返回自定义的错误提示, unwrap_or(T)有值就返回成员，没值就返回T
+- Result<T,E>来处理错误的情况 Ok(&T) Err(&T)
+
 
 # 关键字
 - fn 定义函数
--- rust_test/create_car/src/main.rs
+++ rust_test/create_car/src/main.rs
@@ -1,74 +1,38 @@
-#[derive(PartialEq, Debug)]
-// Declare Car struct to describe vehicle with four named fields
-struct Car {
-    color: String,
-    motor: Transmission,
-    roof: bool,
-    age: (Age, u32),
-}
+use std::fs::File;
+use std::io::{Error, Read};
+use std::path::PathBuf;
 
-#[derive(PartialEq, Debug)]
-// Declare enum for Car transmission type
-enum Transmission { Manual, SemiAuto, Automatic }
+fn read_file_contents(path: PathBuf) -> Result<String, Error> {
+    let mut string = String::new();
 
-#[derive(PartialEq, Debug)]
-enum Age {New, Used}
+    // Access a file at a specified path
+    // ---------------------------------
+    // TODO #1:
+    // - Pass variable to `file` variable on success, or
+    // - Return from function early if there's an error
+    let mut file: File = match File::open(path) {
+        Ok(file_handle) => file_handle,
+        Err(io_error) => return Err(io_error),
+    };
 
+    // Read file contents into `String` variable with `read_to_string`
+    // ---------------------------------
+    // Success path is already filled in
+    // TODO #2: Return from function early if there's an error
+    match file.read_to_string(&mut string) {
+        Ok(_) => (),
+        Err(io_error) => return Err(io_error),
+    };
 
-fn car_quality (miles: u32) -> (Age, u32) {
-    if miles > 0 {
-        (Age::Used, miles)
-    }
-    else {
-        (Age::New, miles)
-    }
-}
-
-
-fn car_factory(color: String, motor: Transmission, roof: bool, miles: u32) -> Car {
-    if car_quality(miles).0 == Age::Used {
-        if roof {
-            println!("Preparing a used car: {:?}, {}, Hard top, {} miles", motor, color, miles);
-        } else {
-            println!("Preparing a used car: {:?}, {}, Convertible, {} miles", motor, color, miles);
-        }
-    } else {
-        if roof {
-            println!("Building a new car: {:?}, {}, Hard top, {} miles", motor, color, miles);
-        } else {
-            println!("Building a new car: {:?}, {}, Convertible, {} miles", motor, color, miles);
-        }
-    }
-
-    Car {
-        color: color,
-        motor: motor,
-        roof: roof,
-        age: car_quality(miles)
-    }
+    // TODO #3: Return `string` variable as expected by function signature
+    Ok(string)
 }
 
 fn main() {
-    // Create car color array
-    let colors = ["Blue", "Green", "Red", "Silver"];
-
-    // Declare the car type and initial values
-    let mut car: Car = Car {
-        color: colors[0].to_string(),
-        motor: Transmission::Manual,
-        roof: false,
-        age: car_quality(0),
-    };
-    let mut engine: Transmission = Transmission::Manual;
-
-    // Car order #1: New, Manual, Hard top
-    car = car_factory(String::from(colors[0]), engine, true, 0);
-
-    // Car order #2: Used, Semi-automatic, Convertible
-    engine = Transmission::SemiAuto;
-    car = car_factory(String::from(colors[1]), engine, false, 100);
-
-    // Car order #3: Used, Automatic, Hard top
-    engine = Transmission::Automatic;
-    car = car_factory(String::from(colors[2]), engine, true, 200);
+    if read_file_contents(PathBuf::from("src/main.rs")).is_ok() {
+        println!("The program found the main file.");
+    }
+    if read_file_contents(PathBuf::from("non-existent-file.txt")).is_err() {
+        println!("The program reported an error for the file that doesn't exist.");
+    }
 }
